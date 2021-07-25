12257be20239c3be2be08739db79fd2ee9206c8d
iUseYahoo <64442818+iUseYahoo@users.noreply.github.com>
2021-07-25 13:08:25 +1000

    fixed

--- hootspam.js
+++ hootspam.js
@@ -1,5 +1,3 @@
-// You need to run this command: npm i kahoot-spammer -g
-// You need to run this command: npm i chalk --save --global
 const KahootSpam = require('kahoot-spammer')
 const chalk = require('chalk');
 const log = console.log;
@@ -10,7 +8,9 @@ const ReadLine = require("readline").createInterface({
 
 var username = 'name';
 var kahootcode = '';
+var amount = 10;
 
+console.clear();
 log(chalk.blueBright(`========================================================================================
 =                                                                                      =
 =      ██╗  ██╗ ██████╗  ██████╗ ████████╗    ███████╗██████╗  █████╗ ███╗   ███╗      =
@@ -26,11 +26,8 @@ async function Main(){
     ReadLine.question("Kahoot Code: ", KC =>{
         kahootcode = KC
         
-        for (i = 0; i < 10; i++){
-            let api = KahootSpam
-            api.spam(kahootcode, username, i)
-            console.log(`[${i}] joined ${kahootcode} as ${username}`)
-        }
+        let api = KahootSpam
+        api.spam(kahootcode, username, amount);
     })
 };
 Main().catch(error => {
